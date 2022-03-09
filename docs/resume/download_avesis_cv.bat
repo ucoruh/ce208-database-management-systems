50883eced53302cc637a4837315841f425a1b716
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-03-10 00:17:13 +0300

    cv name-surname updated

--- docs/resume/download_avesis_cv.bat
+++ docs/resume/download_avesis_cv.bat
@@ -2,8 +2,8 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-set name="ugur"
-set surname="coruh"
+set name="yildiran"
+set surname="yilmaz"
 
 curl -o "%name%_%surname%_tr_cv.pdf" "https://avesis.erdogan.edu.tr/%name%.%surname%/indir?languageCode=tr"
 curl -o "%name%_%surname%_en_cv.pdf" "https://avesis.erdogan.edu.tr/%name%.%surname%/indir?languageCode=en"
